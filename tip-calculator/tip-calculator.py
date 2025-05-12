14d914ae8b13ba628b9a2d9da4dad666004307eb
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-05-12 14:01:08 -0400

    Initial commit/setup for tip calculator exercise

---
+++ tip-calculator/tip-calculator.py
@@ -0,0 +1,7 @@
+amt = 0.00
+
+print("Welcome to the tip calculator!")
+bill_total = input("What was the total bill? ")
+tip_percentage = input("How much would you like to tip? 10, 15, 18, or 20? ")
+party = input("how many people will be splitting the bill? ")
+print(f"Each person should pay: {amt}")
