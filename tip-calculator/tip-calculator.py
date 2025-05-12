b68721388384762d9668903a17c30472bbfcc1c0
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-05-12 14:20:01 -0400

    Add type conversions and formatting

--- tip-calculator/tip-calculator.py
+++ tip-calculator/tip-calculator.py
@@ -1,7 +1,8 @@
 amt = 0.00
 
 print("Welcome to the tip calculator!")
-bill_total = input("What was the total bill? ")
-tip_percentage = input("How much would you like to tip? 10, 15, 18, or 20? ")
-party = input("how many people will be splitting the bill? ")
-print(f"Each person should pay: {amt}")
+bill_total = float(input("What was the total bill? $"))
+tip_percentage = int(input("How much would you like to tip? 10, 15, 18, or 20? "))
+people = int(input("How many people will be splitting the bill? "))
+amt = round((6.788888808), 2)
+print(f"Each person should pay: ${amt}")
