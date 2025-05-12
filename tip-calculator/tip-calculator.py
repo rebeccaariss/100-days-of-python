8904a9c1e49b7d6c8cfe2d880eb00a02538a3cce
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-05-12 14:42:52 -0400

    Always include 2 decimal places (.5 should be .50, for example)

--- tip-calculator/tip-calculator.py
+++ tip-calculator/tip-calculator.py
@@ -4,4 +4,4 @@ tip = int(input("How much would you like to tip? 10, 15, 18, or 20? "))
 people = int(input("How many people will be splitting the bill? "))
 total_with_tip = bill_total * (1 + tip / 100)
 bill_per_person = total_with_tip / people
-print(f"🧾 Each person should pay: ${round(bill_per_person, 2)} 💸")
+print(f"🧾 Each person should pay: ${round(bill_per_person, 2):.2f} 💸")
