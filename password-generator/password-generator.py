260ea23389ea3216f5304b5258c794d04ead9e4a
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-05-22 23:48:57 -0400

    Adjust variable names, add solution for level 1 objective

--- password-generator/password-generator.py
+++ password-generator/password-generator.py
@@ -3,17 +3,18 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
 
-number_of_letters = int(input('How many letters would you like in your password? '))
-number_of_numbers = int(input('How many numbers would you like in your password? '))
-number_of_symbols = int(input('How many symbols would you like in your password? '))
-
-print(letters, numbers, symbols)
+num_letters = int(input('How many letters would you like in your password? '))
+num_numbers = int(input('How many numbers would you like in your password? '))
+num_symbols = int(input('How many symbols would you like in your password? '))
 
 # Level 1:
 sequential_pwd = ''
-for x in range(number_of_letters):
-  # sequential_pwd += letters[random.randint(0, 51)]
+for x in range(num_letters):
   sequential_pwd += random.choice(letters)
+for x in range(num_numbers):
+  sequential_pwd += random.choice(numbers)
+for x in range(num_symbols):
+  sequential_pwd += random.choice(symbols)
 
 print(f"Your password is {sequential_pwd}")
 
