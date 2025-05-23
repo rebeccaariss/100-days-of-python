b192708a8667fb60ac83b4823caa69b48c9a6b93
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-05-22 23:17:24 -0400

    Initial commit

---
+++ password-generator/password-generator.py
@@ -0,0 +1,5 @@
+letters = input('How many letters would you like in your password? ')
+numbers = input('How many numbers would you like in your password? ')
+symbols = input('How many symbols would you like in your password? ')
+
+print(letters, numbers, symbols)
