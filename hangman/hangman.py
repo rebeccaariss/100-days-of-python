d0ac851c0e4934ca88677e131a2e0a7a2842ca9e
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-06-23 23:33:31 -0400

    Generate/test placeholder blanks (underscores)

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,11 +2,13 @@ import random
 
 #Step 1
 # Word list from https://randomwordgenerator.com/
-# word_list = [ 'bubble', 'dilemma', 'grounds', 'revoke', 'condition', 'photocopy', 'notion', 'coalition', 'coincidence', 'constitutional', 'magazine', 'transparent', 'executive', 'bottom', 'housing', 'rotten', 'goalkeeper', 'colourful', 'invite', 'education', 'skilled', 'praise', 'visual', 'security', 'ignore', 'latest', 'oceanic', 'command', 'applied', 'document', 'launch', 'transaction', 'throat', 'tolerate', 'concentrate', 'emotion', 'invasion', 'throne', 'dilute', 'program', 'quantity', 'locate', 'guerrilla', 'species', 'course', 'promise', 'approve', 'school', 'mirror', 'conscience', ]
-word_list = ['mississippi']
+word_list = [ 'bubble', 'dilemma', 'grounds', 'revoke', 'condition', 'photocopy', 'notion', 'coalition', 'coincidence', 'constitutional', 'magazine', 'transparent', 'executive', 'bottom', 'housing', 'rotten', 'goalkeeper', 'colourful', 'invite', 'education', 'skilled', 'praise', 'visual', 'security', 'ignore', 'latest', 'oceanic', 'command', 'applied', 'document', 'launch', 'transaction', 'throat', 'tolerate', 'concentrate', 'emotion', 'invasion', 'throne', 'dilute', 'program', 'quantity', 'locate', 'guerrilla', 'species', 'course', 'promise', 'approve', 'school', 'mirror', 'conscience', ]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
+placeholder = '_' * len(chosen_word)
+print(chosen_word)
+print(placeholder)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input('Please guess a letter: ').lower()
@@ -20,3 +22,8 @@ if guess in chosen_word:
   # print(range(len(guess)))
 else:
   print("No")
+
+
+# 
+  #TODO-4 - Create a "placeholder" with the same number of blanks as the chosen_word
+  #TODO-5 - Create a "display" that puts the guess letter in the right position
