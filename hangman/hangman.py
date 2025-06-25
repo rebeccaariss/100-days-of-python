ed3d943a60c3b58c5a4478147f0846dfbf5d1150
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-06-24 23:47:21 -0400

    Switch to range fn to generate placeholder string; generate display for guesses using loop

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -6,9 +6,10 @@ word_list = [ 'bubble', 'dilemma', 'grounds', 'revoke', 'condition', 'photocopy'
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 placeholder = ''
-for letter in chosen_word:
-  placeholder = placeholder + '_'
+for position in range(word_length):
+  placeholder += '_'
 print(chosen_word)
 print(placeholder)
 
@@ -16,16 +17,22 @@ print(placeholder)
 guess = input('Please guess a letter: ').lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-  print("Yes")
-  print(chosen_word)
-  print(len(chosen_word))
-  print(chosen_word.index(guess))
-  # print(range(len(guess)))
-else:
-  print("No")
+# if guess in chosen_word:
+#   print(chosen_word.index(guess))
+#   index = chosen_word.index(guess)
+#   # print(range(len(guess)))
+# else:
+#   print("No")
 
+display = ''
 
+for letter in chosen_word:
+  if letter == guess:
+    display += letter
+  else:
+    display += '_'
+
+print(display)
 # 
   #TODO-4 - Create a "placeholder" with the same number of blanks as the chosen_word
   #TODO-5 - Create a "display" that puts the guess letter in the right position
